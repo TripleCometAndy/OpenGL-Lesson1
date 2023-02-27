5e6a8532bd7b0f6f32180f902e46d4566dfac598
Andy Langer <mightydodongo@gmail.com>
2023-02-26 19:29:26 -0600

    WIP

--- src/main.cpp
+++ src/main.cpp
@@ -73,10 +73,25 @@ int main() {
         0, 1, 3, // first Triangle
         1, 2, 3  // second Triangle
     };
-    unsigned int VBO, VAO, EBO;
+
+    float vertices2[] = {
+        0.1f,  0.1f,  0.0f, // top right
+        0.1f,  -0.1f, 0.0f, // bottom right
+        -0.1f, -0.1f, 0.0f, // bottom left
+        -0.1f, 0.1f,  0.0f  // top left
+    };
+    unsigned int indices2[] = {
+        // note that we start from 0!
+        0, 1, 3, // first Triangle
+        1, 2, 3  // second Triangle
+    };
+
+    unsigned int VBO, VAO, EBO, VBO2, EBO2;
     glGenVertexArrays(1, &VAO);
     glGenBuffers(1, &VBO);
     glGenBuffers(1, &EBO);
+    glGenBuffers(1, &VBO2);
+    glGenBuffers(1, &EBO2);
     // bind the Vertex Array Object first, then bind and set vertex buffer(s),
     // and then configure vertex attributes(s).
     glBindVertexArray(VAO);
@@ -86,6 +101,13 @@ int main() {
 
     glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
     glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices,
+                 GL_STATIC_DRAW);
+
+    glBindBuffer(GL_ARRAY_BUFFER, VBO2);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices2), vertices2, GL_STATIC_DRAW);
+
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO2);
+    glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices2), indices2,
                  GL_STATIC_DRAW);
 
     glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float),
